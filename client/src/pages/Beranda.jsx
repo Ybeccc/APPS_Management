fc31f998ddbf72c0cabb20240d9c089ca1d2c898
flie2u <fernandolie.dev@gmail.com>
2024-10-26 15:58:28 +0700

    beranda and debug

--- client/src/pages/Beranda.jsx
+++ client/src/pages/Beranda.jsx
@@ -1,13 +1,30 @@
-import React from 'react'
-import Welcome from '../components/Welcome'
-import Layout from './Layout'
+import React, { useEffect } from "react";
+import Layout from "./Layout";
+import Welcome from "../components/Welcome";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { getMe } from "../features/authSlice";
 
 const Beranda = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const { isError } = useSelector((state) => state.auth);
+
+  useEffect(() => {
+    dispatch(getMe());
+  }, [dispatch]);
+
+  useEffect(() => {
+    if (isError) {
+      navigate("/");
+    }
+  }, [isError, navigate]);
+
   return (
     <Layout>
       <Welcome />
     </Layout>
-  )
-}
+  );
+};
 
-export default Beranda
+export default Beranda;
